13209c3644118be4c2648248ed63c1fb4dba749b
leerlandais <leerlandais@gmail.com>
2024-02-29 18:37:26 +0100

    complete rework of html/css. Added JS

--- view/inc/header.php
+++ view/inc/header.php
@@ -1,3 +1,4 @@
 <div class="headerDiv">
     <h1 id="portalHeader">Bienvenue sur mon Portail</h1>
+    <h6 id="screenwidth"></h6>
 </div>
--- view/portailHome.php
+++ view/portailHome.php
@@ -14,18 +14,43 @@ include("../view/inc/header.php");
 ?>
 
 <fieldset>
-    <legend>Choissisez votre Passage</legend>
+    <legend>Choisissez votre Passage</legend>
     <div class="portalHolder">
-      <div class="windowHolder"><h3>Site Préformation</h3><a href="https://2023.webdev-cf2m.be/Lee/Site/" target="_blank"><div class="portalWindow windowPrefo" id="portalToPrefo"></div></a></div> 
-      <div class="windowHolder"><h3>Mes débuts avec JS</h3><a href="https://2023.webdev-cf2m.be/Lee/javaStuff/" target="_blank"><div class="portalWindow windowEarlyJS" id="portalToEarlyJS"></div></a></div> 
-      <div class="windowHolder"><h3>Premier Travail d'Intégration</h3><a href="https://2023.webdev-cf2m.be/Lee/TI/public/" target="_blank"><div class="portalWindow windowTI1" id="portalToTI1"></div></a></div> 
-      <div class="windowHolder"><h3>Deuxième Travail d'Intégration</h3><a href="https://2023.webdev-cf2m.be/Lee/TI2-HomeVersion/public/" target="_blank"><div class="portalWindow windowTI2" id="portalToTI2"></div></a></div> 
-      <div class="windowHolder"><h3>To Be Announced</h3><a href="#"><div class="portalWindow windowTBA1" id="portalToTBA1"></div></a></div> 
-      <div class="windowHolder"><h3>Coming Soon</h3><a href="#"><div class="portalWindow windowTBA2" id="portalToTBA2"></div></a></div> 
-      <div class="windowHolder"><h3>GitHub</h3><a href="https://github.com/Leerlandais" target="_blank"><div class="portalWindow windowGit" id="portalToGit"></div></a></div> 
-      <div class="windowHolder"><h3>CF2M</h3><a href="https://www.cf2m.be/home" target="_blank"><div class="portalWindow windowCF23" id="portalToCF2M"></div></a></div> 
+        <div class="windowHolder" id="windowHolder1">
+            <h3>Site Préformation</h3>
+            <div class="portalWindow windowPrefo" id="portalToPrefo"></div>
+        </div>
+        <div class="windowHolder" id="windowHolder2">
+            <h3>Mes débuts avec JS</h3>
+            <div class="portalWindow windowEarlyJS" id="portalToEarlyJS"></div>
+        </div>
+        <div class="windowHolder" id="windowHolder3">
+            <h3>Premier Travail d'Intégration</h3>
+            <div class="portalWindow windowTI1" id="portalToTI1"></div>
+        </div>
+        <div class="windowHolder" id="windowHolder4">
+            <h3>Deuxième Travail d'Intégration</h3>
+            <div class="portalWindow windowTI2" id="portalToTI2"></div>
+        </div>
+        <div class="windowHolder" id="windowHolder5">
+            <h3>To Be Announced</h3>
+            <div class="portalWindow windowTBA1" id="portalToTBA1"></div>
+        </div>
+        <div class="windowHolder" id="windowHolder6">
+            <h3>Coming Soon</h3>
+            <div class="portalWindow windowTBA2" id="portalToTBA2"></div>
+        </div>
+        <div class="windowHolder" id="windowHolder7">
+            <h3>GitHub</h3>
+            <div class="portalWindow windowGit" id="portalToGit"></div>
+        </div>
+        <div class="windowHolder" id="windowHolder8">
+            <h3>CF2M</h3>
+            <div class="portalWindow windowCF23" id="portalToCF2M"></div>
+        </div>
     </div>
 </fieldset>
+
     
 
 <script src="scripts/script.js"></script>
