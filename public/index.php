18d5b69338c0ae8b90dc62ac0e378ebc5abcd579
leerlandais <leerlandais@gmail.com>
2024-03-06 19:13:13 +0100

    fixed ?pgpggp=

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@ try {
     $db = new PDO(DB_DRIVER . ":host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=" . DB_CHARSET . ";port=" . DB_PORT, DB_LOGIN, DB_PWD);
 } catch (Exception $e) {
     die($e->getMessage());
-}
+}    
 
 $messages = getMessages($db);
 $replies = getReplies($db);
@@ -22,9 +22,9 @@ if (isset($_POST['name'], $_POST['email'], $_POST['message'])) {
         exit();
     } else {        
         $messageError = "Something went wrong";
-    }
+    }    
     
-}
+}    
 
 
 if (isset($_POST['nameRep'], $_POST['parent_id'], $_POST['messageRep'])) {
@@ -37,20 +37,19 @@ if (isset($_POST['nameRep'], $_POST['parent_id'], $_POST['messageRep'])) {
         exit();
     } else {        
         $messageError = "Something went wrong";
-    }
+    }    
     
-}
-
+}    
 
 if(isset($_GET["p"])){
     switch($_GET["p"]){
         case 'home':
-            $title = "Page d'Accueil";
+            $title = "Welcome";
             include("../view/portailHome.php");
             break;
         case 'contact' :
             $title = "Contact Me";
-            include("../view/portailContact.php"); // I'll change it back once the page is done
+            include("../view/portailContact.php");
             break;
         }
     }else{
@@ -58,4 +57,5 @@ if(isset($_GET["p"])){
         include("../view/portailHome.php");
 
 }
+
 
--- view/inc/header.php
+++ view/inc/header.php
@@ -1,20 +1,21 @@
 <div class="headerDiv">
     <h1 id="portalHeader">Bienvenue sur mon Portail</h1>
-   <!-- <h6 id="screenwidth"></h6> -->
+    <!-- <h6 id="screenwidth"></h6> -->
    <?php
-   if (isset($_GET["p"])) {
-    switch($_GET["p"]){
-        case 'home':
-    ?>  <a href="?p=contact"><h4>Contact</h4></a>   <?php
-            break;
-        case 'contact' :
-    ?>  <a href="?p=home"><h4>Home</h4></a>   <?php
-            break;
-        default:
-         ?>  <a href="?p=contact"><h4>Contact</h4></a>   <?php
+    if (isset($_GET["p"])) {
+        switch($_GET["p"]) {
+            case 'home':
+                ?><a href="?p=contact"><h4>Contact</h4></a><?php
+                break;
+            case 'contact':
+                ?><a href="?p=home"><h4>Home</h4></a><?php
+                break;
+                default:
+                header("Location: ?p=home");
+                exit();
         }
-    }else {
-    ?>  <a href="?p=contact"><h4>Contact</h4></a>   <?php
+    } else {
+        ?><a href="?p=contact"><h4>Contact</h4></a><?php
     }
     ?>
 </div>
