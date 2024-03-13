b789a6b800e7f2ff4aecfb0922edf5d1a5e56dc3
leerlandais <leerlandais@gmail.com>
2024-03-13 18:56:17 +0100

    merge prepare

--- view/portailCountries.php
+++ view/portailCountries.php
@@ -15,14 +15,22 @@ include("../view/inc/header.php");
 <h1>Somewhat Complete List of Countries</h1>
 <h2>Things to do 
     <ul>
-        <li>Add Flags</li>
         <li>Select Display Amount</li>
         <li>Sort by anything</li>
         <li>Select countries by X to Y</li>
         <li>Whatever else I can think of</li>
     </ul> 
 </h2>
+<!--
+<form action="" method="POST">
+    <input type="text" name="amount">
+    <button type="submit">change</button>
+</form>
+-->
+<?php
 
+if (isset($pagination)) {
+    echo "<p>$pagination</p>"; }  ?>
 <table class="countryTable">
     <th class="countryInfo">Name</th>
     <th class="countryInfo">ISO Code</th>
@@ -31,19 +39,19 @@ include("../view/inc/header.php");
     <th class="countryInfo">Capital</th>
     <th class="countryInfo">Population of Capital</th>
     <th class="countryInfo">Altitude of Capital</th>
-    <th class="countryInfo">Flag (not yet included)</th>
+    <th class="countryInfo">Flag</th>
     <?php
         foreach($countries as $country) {
     ?>
         <tr class="countryRow">
-            <td class="countryCol"><?php echo $country['nom']; ?></td>
-            <td class="countryCol"><?php echo $country['iso']; ?></td>
+            <td class="countryCol"><?= $country['nom']; ?></td>
+            <td class="countryCol"><?= $country['iso']; ?></td>
             <td class="countryCol"><?= $country['population'];?></td>
             <td class="countryCol"><?= $country['superficie']." km²";?></td>
             <td class="countryCol"><?= $country['capitale'];?></td>
             <td class="countryCol"><?= $country['popu_cap'];?></td>
             <td class="countryCol"><?= $country['altitude']." m";?></td>
-            <td class="countryCol">Flag Coming</td>
+            <td class="countryCol"><img src="../images/svg/<?= strtolower($country['iso']); ?>.svg"></td>
         </tr>
     <?php
         }
