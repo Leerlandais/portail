76d65bd7b47223c59e6388a85294e9ebb733f85f
leerlandais <leerlandais@gmail.com>
2024-05-05 17:47:18 +0200

    DB OK

--- view/portailHome.php
+++ view/portailHome.php
@@ -21,46 +21,16 @@ if (isset($_SESSION['id']) && $_SESSION["id"] === session_id()) {
 <fieldset>
     <legend>Choisissez votre Passage</a></legend>
     <div class="portalHolder">
-    <div class="windowHolder" id="windowHolder10">
-            <h3>CRUD Project</h3>
-            <div class="portalWindow windowCrudProject" id="portalToCrudProject"><div class="portalDesc" id="descCrudProject">Projet sur CRUD, Bootstrap et Carte. Crée le 17/04/2024-21/04/2024</div></div>
-        </div>
-        <div class="windowHolder" id="windowHolder5">
-            <h3>Jeux du Mémoire</h3>
-            <div class="portalWindow windowCardMem" id="portalToCardMem"><div class="portalDesc" id="descCardMem">Jeux du Mémoire. Crée le 07/03/2024-09/03/2024</div></div>
-        </div>
-        <div class="windowHolder" id="windowHolder6">
-            <h3>Countries</h3>
-            <div class="portalWindow windowCountries" id="portalToCountries"><div class="portalDesc" id="descCountries">Un exercice sur les DB et PHP</div></div>
-        </div>
-        <div class="windowHolder" id="windowHolder9">
-            <h3>Tabs</h3>
-            <div class="portalWindow windowTabs" id="portalToTabs"><div class="portalDesc" id="descTabs">Un collection de mes tablatures préférés</div></div>
-        </div>
-        <div class="windowHolder" id="windowHolder1">
-            <h3>Site Préformation</h3>
-            <div class="portalWindow windowPrefo" id="portalToPrefo"><div class="portalDesc" id="descPrefo">Mon premier site. Fait pour le fin de notre préformation (17/11/2023)</div></div>
-        </div>
-        <div class="windowHolder" id="windowHolder2">
-            <h3>Mes débuts avec JS</h3>
-            <div class="portalWindow windowEarlyJS" id="portalToEarlyJS"><div class="portalDesc" id="descEarlyJS">Mes premiers pas en JS (17/11/2023 - 22/12/2023)</div></div>
-        </div>
-        <div class="windowHolder" id="windowHolder3">
-            <h3>Premier Travail d'Intégration</h3>
-            <div class="portalWindow windowTI1" id="portalToTI1"><div class="portalDesc" id="descTI1">Notre premier test d'intégration (14/12/2023)</div></div>
-        </div>
-        <div class="windowHolder" id="windowHolder4">
-            <h3>Deuxième Travail d'Intégration</h3>
-            <div class="portalWindow windowTI2" id="portalToTI2"><div class="portalDesc" id="descTI2">Le deuxième test d'intégration (19/02/2024)</div></div>
-        </div>
-        <div class="windowHolder" id="windowHolder7">
-            <h3>GitHub</h3>
-            <div class="portalWindow windowGit" id="portalToGit"><div class="portalDesc" id="descGit">Vers ma page GitHub</div></div>
-        </div>
-        <div class="windowHolder" id="windowHolder8">
-            <h3>CF2M</h3>
-            <div class="portalWindow windowCF2M" id="portalToCF2M"><div class="portalDesc" id="descCF2M">Vers le site de l'école</div></div>
-        </div>
+        <?php
+        foreach ($getPortals as $port) {
+            ?>
+            <div class="windowHolder">
+                <h3><?=$port["title"]?></h3>
+            <a href="<?=$port["dest_url"]?>"><div class="portalWindow" id="" style="background-image: <?=$port["img_src"]?>;"><div class="portalDesc"><?=$port["description"]?></div></div></a>
+            </div>
+        <?php
+        }
+        ?>
     </div>
 </fieldset>       
     <a href="?login">here</a>
@@ -68,6 +38,6 @@ if (isset($_SESSION['id']) && $_SESSION["id"] === session_id()) {
         if (isset($_GET["login"])) include ("inc/login-form.php");
     ?>
 
-<script src="scripts/script.js"></script>
+<!-- <script src="scripts/script.js"></script> -->
 </body>
 </html>
